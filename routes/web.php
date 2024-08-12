091e01e232f00a4965f06d86ad7d229baabd0b70
kenichi.furuya <ken.f.pooh@gmail.com>
2024-08-12 04:53:40 +0000

    CRUD messages

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessagesController;
+// デフォルトのコメント部分は省略
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', [MessagesController::class, 'index']);
+Route::resource('messages', MessagesController::class);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// CRUD
+// メッセージの個別詳細ページ表示
+//Route::get('messages/{id}', [MessagesController::class, 'show']);
+// メッセージの新規登録を処理（新規登録画面を表示するためのものではありません）
+Route::post('messages', [MessagesController::class, 'store']);
+// メッセージの更新処理（編集画面を表示するためのものではありません）
+//Route::put('messages/{id}', [MessagesController::class, 'update']);
+// メッセージを削除
+//Route::delete('messages/{id}', [MessagesController::class, 'destroy']);
+
+// index: showの補助ページ
+Route::get('messages', [MessagesController::class, 'index'])->name('messages.index');
+// create: 新規作成用のフォームページ
+Route::get('messages/create', [MessagesController::class, 'create'])->name('messages.create');
+// edit: 更新用のフォームページ
+//Route::get('messages/{id}/edit', [MessagesController::class, 'edit'])->name('messages.edit');
