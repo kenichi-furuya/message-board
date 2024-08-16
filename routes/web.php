2fdc66248c91d5f44d24601b826270af3ccc25f6
kenichi-furuya <ken.f.pooh@gmail.com>
2024-08-16 15:20:22 +0000

    validation

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MessagesController;
-// デフォルトのコメント部分は省略
+
+/*
+（コメント部分は省略）
+*/
 
 Route::get('/', [MessagesController::class, 'index']);
 Route::resource('messages', MessagesController::class);
 
 // CRUD
-// メッセージの個別詳細ページ表示
-//Route::get('messages/{id}', [MessagesController::class, 'show']);
 // メッセージの新規登録を処理（新規登録画面を表示するためのものではありません）
-Route::post('messages', [MessagesController::class, 'store']);
+//Route::get('messages/{id}', [MessagesController::class, 'show']);
 // メッセージの更新処理（編集画面を表示するためのものではありません）
 //Route::put('messages/{id}', [MessagesController::class, 'update']);
 // メッセージを削除
 //Route::delete('messages/{id}', [MessagesController::class, 'destroy']);
-
 // index: showの補助ページ
-Route::get('messages', [MessagesController::class, 'index'])->name('messages.index');
+//Route::get('messages', [MessagesController::class, 'index'])->name('messages.index');
 // create: 新規作成用のフォームページ
-Route::get('messages/create', [MessagesController::class, 'create'])->name('messages.create');
+//Route::get('messages/create', [MessagesController::class, 'create'])->name('messages.create');
 // edit: 更新用のフォームページ
 //Route::get('messages/{id}/edit', [MessagesController::class, 'edit'])->name('messages.edit');
